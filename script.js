977bf000c01ef9c4e5f601ed2eaf93442a9178af
TausifReza22 <tausif22@navgurukul.org>
2024-04-04 16:53:03 +0530

    Colorpicker

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let colorDisplay= document.getElementById('container');
+function red(){
+    colorDisplay.style.backgroundColor='red';
+}
+function blue(){
+    colorDisplay.style.backgroundColor='blue';
+}
+function green(){
+    colorDisplay.style.backgroundColor='green';
+}
+function yellow(){
+    colorDisplay.style.backgroundColor='yellow';
+}
+function purple(){
+    colorDisplay.style.backgroundColor='purple';
+}
+function orange(){
+    colorDisplay.style.backgroundColor='orange';
+}
+function pink(){
+    colorDisplay.style.backgroundColor='pink';
+}
+function cyan(){
+    colorDisplay.style.backgroundColor='cyan';
+}
